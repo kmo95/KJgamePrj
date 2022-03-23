98d56f46bb58649bfaa0ede6bd73ac0157ff3a9f
unexistentman <chan3358@naver.com>
2022-03-23 23:48:47 +0900

    add move

--- js/test.js
+++ js/test.js
@@ -1,29 +1,30 @@
 const $box = document.getElementById("box");
 
+const offset = { x: -25, y: -25 };
+
 window.addEventListener("keydown", function (event) {
   if (event.defaultPrevented) {
     return; 
   }
 
-  switch (event.key) {
-  case "Down":
-  case "ArrowDown":
+  if (event.key === "ArrowDown") {
     $box.style.backgroundColor = "red";
-    break;
-  case "Up": 
-  case "ArrowUp":
+    $box.style.transform 
+      = `translate3d(${offset.x}px, ${offset.y = offset.y + 5}px, 0)`;
+  } else if (event.key === "ArrowUp") {
     $box.style.backgroundColor = "blue";
-    break;
-  case "Left": 
-  case "ArrowLeft":
+    $box.style.transform 
+      = `translate3d(${offset.x}px, ${offset.y = offset.y - 5}px, 0)`;
+  } else if (event.key === "ArrowLeft") {
     $box.style.backgroundColor = "green";
-    break;
-  case "Right": 
-  case "ArrowRight":
+    $box.style.transform 
+      = `translate3d(${offset.x = offset.x - 5}px, ${offset.y}px, 0)`;
+  } else if (event.key === "ArrowRight") {
     $box.style.backgroundColor = "yellow";
-    break;
-  default:
-    return; 
+    $box.style.transform 
+      = `translate3d(${offset.x = offset.x + 5}px, ${offset.y}px, 0)`;
+  } else {
+    return;
   }
 
   event.preventDefault();
